4090199da1b877ac20665eb935489270266cb496
Botond Balogh <botond.balogh8@gmail.com>
2019-05-02 17:10:45 +0200

    Remove conflict

--- Unicef/Unicef/Models/Answer.cs
+++ Unicef/Unicef/Models/Answer.cs
@@ -9,14 +9,7 @@ namespace Unicef.Models
     {
         public long AnswerId { get; set; }
         public string Description { get; set; }
-<<<<<<< HEAD
         public int Value { get; set; }
-=======
-
-        public int Value { get; set; }
-
-
->>>>>>> bd84a4b5cd0afe6dd5f14f1d0eabad121c3514ee
         public Question Question { get; set; }
 
     }
