61c82eb2dd5d293a99097f91bd73c983351aa877
Fabian Zsofia <fabian.zsofia.eszter@gmail.com>
2019-05-02 13:32:37 +0200

    Unicef models create

---
+++ Unicef/Unicef/Models/Answer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unicef.Models
+{
+    public class Answer
+    {
+        public string Description { get; set; }
+        public bool Value { get; set; }
+    }
+}
